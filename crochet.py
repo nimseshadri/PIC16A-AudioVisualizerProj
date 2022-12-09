b169be00433f8bc5f68b8f6dfd731699f9628eba
nimseshadri <114253680+nimseshadri@users.noreply.github.com>
2022-12-08 16:14:12 -0800

    Update crochet.py

--- crochet.py
+++ crochet.py
@@ -9,60 +9,47 @@ class CrochetDataVisualizer:
         self.df = self.preprocess(df)
     
     def preprocess(self, df):
-        '''
-        Takes in the dataframe that is input. Cleans up data so that it only includes what we plan to be able to work with.
-        Drops unnecessary indexes, makes the dates column into dates python can read and gets rid of additional columns. Gets rid of outiers in the df.
-        Reformats data in columns so that the computer can read it.
-        Args:
-        Clare Cox Crochet dataframe
-        Returns:
-        Clare Cox Crochet dataframe but cleaned up
-        '''
         df.drop(["Unnamed: 0"], axis=1, inplace=True)  # get rid of unnecessary index
         
         df['Date'] = pd.to_datetime(df["Date"])  # make dates
         df.drop(['Day of Week', 'Month', 'Year'], axis=1, inplace=True)  # no longer need this info
         
-        df = df[df["number of pictures in post"] != 'video'].copy()            # get rid of outlier: video
-        # turn percent following column into floats
-        df["percent weren't following"] = df["percent weren't following"].replace({'-': '-1', '': '-1'}).astype(np.float64).replace({-1.:np.nan}) 
-        numerify = ["number of pictures in post", 'profile visits', "follows"] # columns whose datatype should be made into numbers put into an array
-        df[numerify] = df[numerify].astype(np.int16)                           # turns appropriate data in the data frame into numbers (int16)
+        df = df[df["number of pictures in post"] != 'video'].copy()  # get rid of outlier: video
+        df["percent weren't following"] = df["percent weren't following"].replace({'-': '-1', '': '-1'}).astype(np.float64).replace({-1.:np.nan})  # turn percent following column into floats
+        numerify = ["number of pictures in post", 'profile visits', "follows"]
+        df[numerify] = df[numerify].astype(np.int16)
         
-        df["number of days since previous post"] = df['Date'].diff()           # .diff() will get the number of days between each date there was a post
+        df["number of days since previous post"] = df['Date'].diff()
         
-        df["product type"] = df["product type"].fillna("none")                 # replaces null items in product type to 'none'
+        df["product type"] = df["product type"].fillna("none")
         
         general_product_types = [] # create a list that will become a new column in the dataframe with the more general categories of 
-        
-        # loop through all the items in the product type column and search for substrings to put them in a more general category of product
+     
         for item in df.loc[:, 'product type']:
-            item = str(item)                          # convert item to a string to check for substrings
-            if 'top' in item:                         # if top is in an item then add top to its index in general_product_types
+            item = str(item)
+            if 'top' in item:
                 general_product_types.append('top')
-            elif 'sweater' in item:                   # if sweater is in the item string then add it as top in general_product_types
+            elif 'sweater' in item:
                 general_product_types.append('top')
-            elif 'hat' in item:                       # if hat is in the item string add it as hat
+            elif 'hat' in item:
                 general_product_types.append('hat')
-            elif 'beanie' in item:                    # if beanie is in the item string at it as hat
+            elif 'beanie' in item:
                 general_product_types.append('hat')
-            elif 'bag' in item:                       # if bag is in the item string add it as bag
+            elif 'bag' in item:
                 general_product_types.append('bag')
-            elif 'pack' in item:                      # if pack is in the item string add it as bag
+            elif 'pack' in item:
                 general_product_types.append('bag')
             else:
-                general_product_types.append('other') # if none of the above strings appear in an item of 'product types' append 'other' to the general_product_types
+                general_product_types.append('other')
         
-        # turn general_product_types into a new column in the data frame
-        df['general_product_types'] = general_product_types 
+        df['general_product_types'] = general_product_types
        
-        # converts categorical variables into dummies where numbers will correstp
-        cats = {"who's featured": None, 'product type': None, 'purpose': None, 'season': None} # a dict of the categorical variables we want to work with
-        for cat in cats:                                                                       # loops through them and gets the dummies of the variables
+        cats = {"who's featured": None, 'product type': None, 'purpose': None, 'season': None}
+        for cat in cats:
             onehot = pd.get_dummies(df[cat])
-            cats[cat] = onehot                                                                 # replaces the categorical items with the dummies
+            cats[cat] = onehot
             
-        df.rename(lambda string: string.replace(" ", "_"), axis=1, inplace=True)               # rename strings in the df and replace spaces with underscores
+        df.rename(lambda string: string.replace(" ", "_"), axis=1, inplace=True)
         
         return df
     
@@ -76,10 +63,10 @@ class CrochetDataVisualizer:
         Raises:
         - KeyError if the column name isn't in self.df
         """
-        for col in cols:      # loop through the columns that were input
-            try:              # first try the column
+        for col in cols:
+            try:
                 self.df[col]
-            except KeyError:  # if there's a KeyError print this
+            except KeyError:
                 raise KeyError(f'"{col}" is not a column in the crochet data')
                 
     def _validate_column_dec(func):
@@ -91,7 +78,7 @@ class CrochetDataVisualizer:
         Returns:
         - the same function with the added validation logic (raises descriptive KeyError when column is not found)
         """
-        @wraps(func)                   
+        @wraps(func)
         def ret_func(self, *pos, **kw):
             self._validate_columns(*pos)
             return func(self, *pos, **kw)
@@ -107,60 +94,89 @@ class CrochetDataVisualizer:
         Returns:
         - None
         Displays:
-        - scatterplot of y vs x
+        - scatterplot of x vs y
         """
-        plt.scatter(self.df[x], self.df[y], color = 'pink')
-        plt.xlabel(x)
-        plt.ylabel(y)
-        plt.title(x + " vs. " + y)
-        plt.show()
+        plt.scatter(self.df[x], self.df[y], color = 'pink') # creates a scatter plot of data for inputted columns, makes plotted dot colors pink
+        plt.xlabel(x) # labels x-axis with x column name
+        plt.ylabel(y) # labels y-axis with y column name
+        plt.title(x + " vs. " + y) # gives graph title 
+        plt.show() # displays graph
         
     @_validate_column_dec
     def timeline(self, y):
-    '''
-    For creating plots which show how a variable changesover time. creates the x-axis and then calls _validate_column_dec
-    Args:
-    y: a column in the data frame which will be plotted in the y axis
-    Returns:
-    Nothing
-    
-    '''
-        plt.scatter(self.df["Date"], self.df[y], color = 'pink')
-        plt.xticks(rotation = 90)
-        plt.xlabel("Date")
-        plt.title(y + " overtime")
-        plt.ylabel(y)
-        plt.show()
+        """
+        Creates a timeline
+        Args:
+        - y: column name
+        Returns:
+        - None
+        Displays:
+        - timeline of y overtime
+        """
+        plt.scatter(self.df["Date"], self.df[y], color = 'pink') # creates a scattered image of Date vs inputted y data, makes plotted dot colors pink
+        plt.xticks(rotation = 90) # x-axis ticks of dates are written out at a 90 deg angle
+        plt.xlabel("Date") # labels x-axis as "Date"
+        plt.title(y + " overtime") # gives graph title 
+        plt.ylabel(y) # labels y-axis with y column name
+        plt.show() # displays graph
         
     @_validate_column_dec
     def histogram(self, *xs):
+        """
+        Creates a histogram
+        Args:
+        - x: column names
+        Returns:
+        - None
+        Displays:
+        - historgram of frequncy of inputted data
+        """
         for x in xs:
-            plt.hist(self.df[x], alpha=0.5, label=x)
-        plt.title("frequency of inputted data")
-        plt.legend()
-        plt.show()
+            plt.hist(self.df[x], alpha=0.5, label=x) # create histogram with frequency of inputted data, gives graph bars transparency of 0.5 and labels axis with x inputted data
+        plt.title("frequency of inputted data") # give graph title
+        plt.legend() # creates a legend indicating which bar on the graph corresponds to which collection of data
+        plt.show() # shows graph
     
     @_validate_column_dec
     def boxplot(self, y, groups=None):
-        if groups is not None:
-            self._validate_columns(groups)
+        """
+        Creates a boxplot
+        Args:
+        - y: column name
+        Returns:
+        - None
+        Displays:
+        - a boxplot with minimum, first quartile, median, third quartile, and maximum of inputted data
+        """
+        if groups is not None: 
+            self._validate_columns(groups) 
             labs, boxes = zip(*((group, df[y]) for group, df in self.df.groupby(groups)))
             plt.boxplot(boxes,labels=labs)
             plt.xlabel(groups)
         else:
-            plt.boxplot(self.df[y])
-        plt.ylabel(y)
-        plt.title(y + " data summary")
-        plt.show()
+            plt.boxplot(self.df[y]) # creates box plot with inputted y column data
+        plt.ylabel(y) # labels y-axis with y data name
+        plt.title(y + " data summary") # gives graph title
+        plt.show() # shows plot
         
     @_validate_column_dec
     def barplot(self, x, y):
-        plt.bar(self.df[x], self.df[y], color = 'pink')
-        plt.xticks(rotation = 90)
-        plt.xlabel(x)
-        plt.ylabel(y)
-        plt.title(x + " vs. " + y)
-        plt.show()
+        """
+        Creates a boxplot
+        Args:
+        - x: column name 
+        - y: column name 
+        Returns:
+        - None
+        Displays:
+        - a barplot of x vs y
+        """
+        plt.bar(self.df[x], self.df[y], color = 'pink') # creates a scatter plot of data for inputted columns, makes plotted dot colors pink
+        plt.xticks(rotation = 90) # x-axis ticks of dates are written out at a 90 deg angle
+        plt.xlabel(x) # labels x-axis with x column name
+        plt.ylabel(y) # labels y-axis with y column name
+        plt.title(x + " vs. " + y) # gives graph title
+        plt.show() # shows plot
 
         
 input_sym = "> "
